3c84f42854163a05df4130e9faf83750397f7328
RSFPO <2733177116@qq.com>
2025-07-17 20:50:43 +0800

    all

---
+++ 011_average/average.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    int a,b;
+
+    scanf("%D %d", &a, &b);
+
+    double c = (a+b)/2.0;
+
+    printf("%d和%d的平均值=%f\n", a, b, c);
+
+    return 0;
+}    //
+// Created by XY on 25-7-13.
+//
