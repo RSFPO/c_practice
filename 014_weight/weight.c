adf055890fa1263881c7f00a35f7914b9de6738a
RSFPO <2733177116@qq.com>
2025-07-21 18:47:53 +0800

    all

---
+++ 014_weight/weight.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    printf("请输入体重的公斤"
+    "如输入\"50\"表示50公斤:");
+
+    int kg;
+
+    scanf("%d",&kg);
+
+    printf("体重是%f斤。\n",
+    (kg*2.0));
+
+    return 0;
+}
+    //
+    // Created by XY on 25-7-21.
+    //
